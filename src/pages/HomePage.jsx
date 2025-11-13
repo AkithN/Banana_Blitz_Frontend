125b1237ace2e262488a7f3ed1a826eddb891889
Akith Nimpura <mpakithn@gmail.com>
2025-11-13 22:06:22 +0530

    up

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/common/navbar";
+import Footer from "../components/common/footer";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <Navbar />
+      HomePage
+      <Footer />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
